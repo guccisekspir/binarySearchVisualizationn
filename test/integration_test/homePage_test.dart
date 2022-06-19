9af6161b72ff0d13fe3b33f0734ef96c45da9f58
Gucci Şekspir <348275@ogr.ktu.edu.tr>
2022-06-19 15:48:58 +0300

    test improvements
    test migrated hardcoded to global edge cases.

--- test/integration_test/homePage_test.dart
+++ test/integration_test/homePage_test.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
@@ -36,15 +38,18 @@ void main() {
 
   testWidgets("is containing number give less lowest number", (WidgetTester widgetTester) async {
     app.main();
+
     await widgetTester.pumpWidget(HomePage(
-      key: ValueKey("HomePage"),
+      key: const ValueKey("HomePage"),
       currentWillSearchList: currentWillSearchList,
     ));
-    await widgetTester.pumpAndSettle(Duration(seconds: 2));
+    //It runs in log time O(log n). So if we have an array of 1024 elements, the maximum number of steps is log21024 = 10 because 210 = 1024
+    int maximumStepOfCurrentList = log(currentWillSearchList.length).floor();
+    await widgetTester.pumpAndSettle(const Duration(seconds: 2));
 
-    await widgetTester.enterText(find.byKey(ValueKey(HomePageKeys.generalTextField)), "13");
-    await widgetTester.tap(find.byKey(ValueKey(HomePageKeys.startSearchButtonKey)));
-    await widgetTester.pumpAndSettle(Duration(seconds: 10));
+    await widgetTester.enterText(find.byKey(const ValueKey(HomePageKeys.generalTextField)), "13");
+    await widgetTester.tap(find.byKey(const ValueKey(HomePageKeys.startSearchButtonKey)));
+    await widgetTester.pumpAndSettle(Duration(seconds: maximumStepOfCurrentList));
 
     expect(find.textContaining("Final result founded : 12"), findsOneWidget);
   });
@@ -52,14 +57,15 @@ void main() {
   testWidgets("is exact number is give same number", (WidgetTester widgetTester) async {
     app.main();
     await widgetTester.pumpWidget(HomePage(
-      key: ValueKey("HomePage"),
+      key: const ValueKey("HomePage"),
       currentWillSearchList: currentWillSearchList,
     ));
-    await widgetTester.pumpAndSettle(Duration(seconds: 2));
+    int maximumStepOfCurrentList = log(currentWillSearchList.length).floor();
 
-    await widgetTester.enterText(find.byKey(ValueKey(HomePageKeys.generalTextField)), "45");
-    await widgetTester.tap(find.byKey(ValueKey(HomePageKeys.startSearchButtonKey)));
-    await widgetTester.pumpAndSettle(Duration(seconds: 10));
+    await widgetTester.pumpAndSettle(const Duration(seconds: 2));
+    await widgetTester.enterText(find.byKey(const ValueKey(HomePageKeys.generalTextField)), "45");
+    await widgetTester.tap(find.byKey(const ValueKey(HomePageKeys.startSearchButtonKey)));
+    await widgetTester.pumpAndSettle(Duration(seconds: maximumStepOfCurrentList));
 
     expect(find.textContaining("Final result founded : 45"), findsOneWidget);
   });
@@ -67,14 +73,18 @@ void main() {
   testWidgets("is lower than lowest variable gives -1", (WidgetTester widgetTester) async {
     app.main();
     await widgetTester.pumpWidget(HomePage(
-      key: ValueKey("HomePage"),
+      key: const ValueKey("HomePage"),
       currentWillSearchList: currentWillSearchList,
     ));
-    await widgetTester.pumpAndSettle(Duration(seconds: 2));
 
-    await widgetTester.enterText(find.byKey(ValueKey(HomePageKeys.generalTextField)), "1");
-    await widgetTester.tap(find.byKey(ValueKey(HomePageKeys.startSearchButtonKey)));
-    await widgetTester.pumpAndSettle(Duration(seconds: 10));
+    int maximumStepOfCurrentList = log(currentWillSearchList.length).floor();
+    int lowerThanLowestNumber = currentWillSearchList.first - 1;
+
+    await widgetTester.pumpAndSettle(const Duration(seconds: 2));
+    await widgetTester.enterText(
+        find.byKey(const ValueKey(HomePageKeys.generalTextField)), lowerThanLowestNumber.toString());
+    await widgetTester.tap(find.byKey(const ValueKey(HomePageKeys.startSearchButtonKey)));
+    await widgetTester.pumpAndSettle(Duration(seconds: maximumStepOfCurrentList));
 
     expect(find.textContaining("Final result founded : -1"), findsOneWidget);
   });
@@ -82,14 +92,17 @@ void main() {
   testWidgets("is higher than last array member will give last array member", (WidgetTester widgetTester) async {
     app.main();
     await widgetTester.pumpWidget(HomePage(
-      key: ValueKey("HomePage"),
+      key: const ValueKey("HomePage"),
       currentWillSearchList: currentWillSearchList,
     ));
-    await widgetTester.pumpAndSettle(Duration(seconds: 2));
+    int maximumStepOfCurrentList = log(currentWillSearchList.length).floor();
+    int higherThanHighestNumber = currentWillSearchList.last + 1;
 
-    await widgetTester.enterText(find.byKey(ValueKey(HomePageKeys.generalTextField)), "69");
-    await widgetTester.tap(find.byKey(ValueKey(HomePageKeys.startSearchButtonKey)));
-    await widgetTester.pumpAndSettle(Duration(seconds: 10));
+    await widgetTester.pumpAndSettle(const Duration(seconds: 2));
+    await widgetTester.enterText(
+        find.byKey(const ValueKey(HomePageKeys.generalTextField)), higherThanHighestNumber.toString());
+    await widgetTester.tap(find.byKey(const ValueKey(HomePageKeys.startSearchButtonKey)));
+    await widgetTester.pumpAndSettle(Duration(seconds: maximumStepOfCurrentList));
 
     expect(find.textContaining("Final result founded : 68"), findsOneWidget);
   });
